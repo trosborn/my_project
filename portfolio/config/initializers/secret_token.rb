2b5fd95ce1bba60bfd63ac984533baa02deee7d6
Thomas Osborn <trosborn@gmail.com>
2014-01-13 22:52:38 -0800

    first commit

---
+++ portfolio/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '40757772d7ea882db6e3951bae1af45de8d0b8cb7f9babf650d9db3a1330f49d0add1e2f012fab31a81b58c320ddf3f8b42bb4c6e58031a3266d13fb020c9034'
---
+++ spec/features/search_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "My search page" do
+	it "has results" do
+		visit "http://google.com"
+		fill_in "q", with: "Code fellows"
+		click_on "Google Search"
+		page.text.must_include "codefellows.org"
+		page.text.must_include "twitter.com/CodeFellowsOrg"
+	end
+end
